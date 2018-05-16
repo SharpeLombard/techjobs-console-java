71f452542981e4318eecb15979ee8fc0216d523a
Mad Hatter <rebecca.s.lombard@gmail.com>
2018-05-15 23:21:01 -0500

    Implemented PrintJobs

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,21 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        //(RSL)System.out.println("printJobs is not implemented yet");
+    //(RSL}
+//(RSL)}
+//adding some new lines hereafter (RSL)
+        if (someJobs.size() > 0) {
+            for(HashMap<String, String> job : someJobs) {
+                    System.out.println("*****");
+                    for(String key : job.keySet()) {
+                            System.out.println(key + ": " + job.get(key));
+                    }
+                    System.out.println("*****");
+             }
+        }
+        else {
+                System.out.println("No jobs found.");
+        }
     }
 }
